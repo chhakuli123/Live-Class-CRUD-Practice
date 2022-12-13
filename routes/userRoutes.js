79548ca5f1b678cc00658df8baaafa0e347615c1
Chhakuli <chhakulizingare12@gmail.com>
2022-12-13 11:08:47 +0530

    created a route file

---
+++ routes/userRoutes.js
@@ -0,0 +1,7 @@
+//url path
+const express = require("express")
+const { home } = require("../controllers/userControllers")
+const router = express.Router()
+
+router.get("/", home)
+module.exports = router;
